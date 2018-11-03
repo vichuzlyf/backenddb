66b4072447f2dc5f42275c0539e24b0cd0bd501f
temp <temp@RILITS-HWLTP007.in.ad.dom>
2018-11-03 11:31:07 +0530

    haii bhagavan

--- src/main/java/com/example/git/sample/bhagavan/Bhagavan.java
+++ src/main/java/com/example/git/sample/bhagavan/Bhagavan.java
@@ -3,6 +3,7 @@ package com.example.git.sample.bhagavan;
 public class Bhagavan {
 	public static void main(String[] args) {
 		System.out.println("I am bhagavan");
+		System.out.println("I am vinay");
 		
 	}
 
