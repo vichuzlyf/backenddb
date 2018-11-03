fe956da684f45812a5cc85c8d67b39ffb4a34c61
Vijay Damisetti <vijaydamisetti@Vijays-MacBook-Pro.local>
2018-11-03 15:32:52 +0530

    vijay commited code through terminal...

---
+++ src/main/java/com/example/batch22a/vijay.java
@@ -0,0 +1,11 @@
+package com.example.batch22a;
+
+public class vijay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("testing though terminal...");
+	}
+
+}
