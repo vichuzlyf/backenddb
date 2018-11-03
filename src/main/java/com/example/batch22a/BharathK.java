bbe7ef9610334793ee92cc6af8ef03b178ca4cb3
karantalasila <karantalasila@gmail.com>
2018-11-03 16:22:46 +0530

    test2 method added

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -6,6 +6,11 @@ public class BharathK {
 	public void test1() {
 		System.out.println("I am bharath");
 	}
+	
+	public void test2() {
+		
+		System.out.println("I am karan in bharath class");
+	}
 }
  
  
