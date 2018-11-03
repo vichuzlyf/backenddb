d2cdc88b688f8a161a130a99e8c35125007775a5
Hari <Hari@192.168.0.28>
2018-11-03 11:32:30 +0530

    hari

--- src/main/java/com/example/git/sample/Hari.java
+++ src/main/java/com/example/git/sample/Hari.java
@@ -2,6 +2,7 @@ package com.example.git.sample;
 
 public class Hari {
 public static void main(String[] args) {
-	System.out.println("hari");
+	System.out.println("hari ");
+	System.out.println("haiii i am slokam ");
 }
 }
