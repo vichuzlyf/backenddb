40ecb1df4c7bb213261426f7605980cf34390cc8
Lenovo <Lenovo@192.168.2.17>
2018-11-03 11:45:29 -0700

    ***

--- src/main/java/com/example/git/sample/Himaja.java
+++ src/main/java/com/example/git/sample/Himaja.java
@@ -2,7 +2,7 @@ package com.example.git.sample;
 
 public class Himaja {
 public static void main(String[] args) {
-	System.out.println("**** HIMAJA ****");
+	System.out.println("*******");
 	System.out.println("slokam");
 }
 }
