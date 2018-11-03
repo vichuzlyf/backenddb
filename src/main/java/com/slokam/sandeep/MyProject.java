9b34b3678620d13ce1befbf962593e1ca2f644e9
sandeepkomera <sandeep.komera@gmail.com>
2018-11-03 15:54:59 +0530

    newwork

--- src/main/java/com/slokam/sandeep/MyProject.java
+++ src/main/java/com/slokam/sandeep/MyProject.java
@@ -4,10 +4,7 @@ public class MyProject {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Friends welcome to the new concept of git");
-		System.out.println("new repository added");
-		System.out.println("started working with gitbash worlk");
-		
+		System.out.println("Work Completed");
 	}
 
 }
