16b6e9ec5ef9815108b022949c5255ed9669c7c6
hemasurapureddy <hemasurapureddy.3597@gmail.com>
2018-11-03 16:17:38 +0530

    hema

--- src/main/java/com/slokam/git/example/Hema2.java
+++ src/main/java/com/slokam/git/example/Hema2.java
@@ -3,7 +3,7 @@ package com.slokam.git.example;
 public class Hema2 {
 
 	public static void main(String[] args) {
-System.out.println("i am hemaa");
+System.out.println("i am slokam student");
 	}
 
 }
