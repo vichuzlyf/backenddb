b7b09810d15a36df7c2f4a41ddff517e2daf325c
temp <temp@RILITS-HWLTP007.in.ad.dom>
2018-11-03 11:06:37 +0530

    bhagavan add new package

--- src/main/java/com/example/git/sample/bhagavan/Bhagavan.java
+++ src/main/java/com/example/git/sample/bhagavan/Bhagavan.java
@@ -2,7 +2,7 @@ package com.example.git.sample.bhagavan;
 
 public class Bhagavan {
 	public static void main(String[] args) {
-		System.out.println("i am bhagavan");
+		System.out.println("I am bhagavan");
 		
 	}
 
