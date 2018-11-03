462b466690b3699147241a79dc198f10b4d19c71
ss <ss@192.168.0.22>
2018-11-03 11:19:22 +0530

    Srinath.java class modified

--- src/main/java/com/example/git/sample/Srinath.java
+++ src/main/java/com/example/git/sample/Srinath.java
@@ -3,7 +3,7 @@ package com.example.git.sample;
 public class Srinath {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		System.out.println("Hello Worlds.");
 
 	}
 
