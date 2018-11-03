1a3dc53386b172823812ecad72e2ebf8f7177b74
harikrishnachunduri <chundurihari1149@gmail.com>
2018-11-03 16:12:39 +0530

    hari

--- src/main/java/com/example/batch22a/Raghu.java
+++ src/main/java/com/example/batch22a/Raghu.java
@@ -3,7 +3,7 @@ package com.example.batch22a;
 public class Raghu {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Slokam...");
+		System.out.println("Hello Slokam students");
 
 	}
 
