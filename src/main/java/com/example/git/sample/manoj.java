933b1d82cba88dad38e8d334d57c3a3b6e237cca
Manoj Kumar <Manoj Kumar@192.168.0.19>
2018-11-03 11:17:48 +0530

    manoj

--- src/main/java/com/example/git/sample/manoj.java
+++ src/main/java/com/example/git/sample/manoj.java
@@ -2,7 +2,7 @@ package com.example.git.sample;
 
 public class manoj {
 	public static void main(String[] args) {
-		System.out.println("i amm  manoj1124");
+		System.out.println("i amm  manoj114");
 	}
 
 }
