641d8db219219717f2d7a936ee498f0ea04c536d
harikrishnachunduri <chundurihari1149@gmail.com>
2018-11-03 17:21:11 +0530

    commited

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -1,201 +1,78 @@
 package com.example.batch22a;
 
 public class BharathK {
-
-<<<<<<< HEAD
 	public  String saveGitHub(){
 		System.out.println("githubb");
 			return "hello";
-		}
+	}	
 
-<<<<<<< HEAD
-	
-=======
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 
 		
 	
-}}
-=======
-		
-	}
-	public void harish() {
-		System.out.println("Harish ...");
-	}
->>>>>>> 7b7e8b5e236d3d6346840b15c136dcf252df0cec
-	public void test1() {
-		System.out.println("I am bharath ");
-		System.out.println("haiii sir");
-	}
-	public void testSM() {
-		System.out.println("I am bharath");
-	}
-	public void sandeep() {
-		System.out.println("Hello folks");
-
-	}
-	public void hari(){
-		System.out.println("hari");
-
-	}
-<<<<<<< HEAD
-	public void abhi() {
-		System.out.println("i am abhi");
-	}
-public void harish() {
-		System.out.println("Harish ...");
-	}
-	
-
-	public void narasimha() {
-		System.out.println("I am narasimha reddy");
-	}
-	public void ghani(){
-		System.out.println("iam ghani");
-
-<<<<<<< HEAD
-	}
-=======
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public void TestName() {
-		
-		System.out.println("I am dayakar reddy");
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> fb00d2f942d58efcb2b8bd6ad1a5d3788d890c82
-
-
-	public void methodByVijay() {
-		System.out.println("method added by vijay and commit through terminal...");
-	}
-	public void santhosh()
-	{
-	}
-
-	
-
->>>>>>> 90c3402b8ae4a421bf2bef27840691f241062591
-	public void saiPurushothamReddy() {
-		System.out.println("SAI ANE NENU");
-	}
-	public void sandeep() {
-		System.out.println("Hello folks");
-	}
-	public void viswanath(){
-		System.out.println("haiiii i am viswanath");
-<<<<<<< HEAD
->>>>>>> 6e88c68276bebac6f818e3c7af1e64a0336315ef
-=======
-<<<<<<< HEAD
-=======
-=======
-public static void main(String[] args) {
-	
-<<<<<<< HEAD
-=======
-	public static void main(String[] args) {
-		
-	}
-	public void harish() {
-		System.out.println("Harish ...");
-	}
->>>>>>> 7b7e8b5e236d3d6346840b15c136dcf252df0cec
-	public void test1() {
-		System.out.println("I am bharath ");
-		System.out.println("haiii sir");
-	}
-	public void testSM() {
-		System.out.println("I am bharath");
-	}
-	public void sandeep() {
-		System.out.println("Hello folks");
-
-	}
-	public void hari(){
-		System.out.println("hari");
-
-	}
-<<<<<<< HEAD
-	public void harihh(){
-		System.out.println("harii new");
-	}
-=======
-<<<<<<< HEAD
-	public void abhi() {
-		System.out.println("i am abhi");
-	}
-public void harish() {
-		System.out.println("Harish ...");
-	}
-	
->>>>>>> bb79e95444a3e06e67530c9a3a2682d5691ed920
-
-	public void narasimha() {
-		System.out.println("I am narasimha reddy");
-	}
-	public void ghani(){
-		System.out.println("iam ghani");
-
-<<<<<<< HEAD
-	}
-=======
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public void TestName() {
-		
-		System.out.println("I am dayakar reddy");
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> fb00d2f942d58efcb2b8bd6ad1a5d3788d890c82
-
-
-	public void methodByVijay() {
-		System.out.println("method added by vijay and commit through terminal...");
-	}
-	public void santhosh()
-	{
-	}
-
-	
-
->>>>>>> 90c3402b8ae4a421bf2bef27840691f241062591
-	public void saiPurushothamReddy() {
-		System.out.println("SAI ANE NENU");
-	}
-	public void sandeep() {
-		System.out.println("Hello folks");
-	}
-	public void viswanath(){
-		System.out.println("haiiii i am viswanath");
-<<<<<<< HEAD
->>>>>>> 6e88c68276bebac6f818e3c7af1e64a0336315ef
-=======
-<<<<<<< HEAD
-=======
-=======
 }
->>>>>>> a94c631623349e62356a6a0190a4ce505a43cc20
+
+	
+	public void harish() {
+		System.out.println("Harish ...");
+	}
+
+	public void test1() {
+		System.out.println("I am bharath ");
+		System.out.println("haiii sir");
+	}
+	public void testSM() {
+		System.out.println("I am bharath");
+	}
+	public void sandeep() {
+		System.out.println("Hello folks");
+
+	}
+	public void hari(){
+		System.out.println("hari");
+
+	}
+
+	public void abhi() {
+		System.out.println("i am abhi");
+	}
 
 	
 
+	public void narasimha() {
+		System.out.println("I am narasimha reddy");
+	}
+	public void ghani(){
+		System.out.println("iam ghani");
+		
+	}
+
+	public void TestName() {
+		
+		System.out.println("I am dayakar reddy");
+
+}
+
+	public void methodByVijay() {
+		System.out.println("method added by vijay and commit through terminal...");
+	}
+	public void santhosh()
+	{
+	}
+
+	
+
+	public void saiPurushothamReddy() {
+		System.out.println("SAI ANE NENU");
+	}
+	
+	public void viswanath(){
+	
+
+	}
+
+	
 	
 	public void Teja() {
 		System.out.println("Hello ");
@@ -204,4 +81,3 @@ public void harish() {
 }
  
  
->>>>>>> 86796da763916b9c1cce856b1be07428900c70b4
