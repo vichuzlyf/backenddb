7a128db208ba04aa65e0094fd0234936dac292b1
Hari <Hari@192.168.0.28>
2018-11-03 11:52:33 +0530

    hari

--- src/main/java/com/example/backendDB/git/ExampleGit.java
+++ src/main/java/com/example/backendDB/git/ExampleGit.java
@@ -4,7 +4,7 @@ public class ExampleGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("haiiii hari");
+System.out.println("haiiii hari java");
 	}
 
 }
