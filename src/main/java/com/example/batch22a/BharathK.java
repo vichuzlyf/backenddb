7de92e9ec8fe3b34eee4ed9856146e0e2acfd3b9
Vijay Damisetti <vijaydamisetti@Vijays-MacBook-Pro.local>
2018-11-03 16:25:52 +0530

    added new method in bharath class

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -6,6 +6,10 @@ public class BharathK {
 	public void test1() {
 		System.out.println("I am bharath");
 	}
+	
+	public void methodByVijay() {
+		System.out.println("method added by vijay and commit through terminal...");
+	}
 }
  
  
