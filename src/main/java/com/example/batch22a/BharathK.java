d683dc79b6ec2f8f266f2f6a31f6c3b99dca098d
sandeepkomera <sandeep.komera@gmail.com>
2018-11-03 16:42:57 +0530

    hello

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -32,9 +32,7 @@ public class BharathK {
 =======
 	public void sandeep() {
 		System.out.println("Hello folks");
->>>>>>> 873f99b0556ffc19ff3a5a54369fb2ded78a8e8b
-=======
-=======
+
 	public void test2() {
 		
 		System.out.println("I am karan in bharath class");
