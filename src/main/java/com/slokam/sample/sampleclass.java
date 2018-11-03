6e2b682ecbf47c0bc690045e2992fddec5c25f8b
sivafullstack <siva91484@gmail.com>
2018-11-03 18:04:48 +0530

    my name is shiva

---
+++ src/main/java/com/slokam/sample/sampleclass.java
@@ -0,0 +1,11 @@
+package com.slokam.sample;
+
+public class sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("helloworld");
+System.out.println("helloworld2");
+	}
+
+}
