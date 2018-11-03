041be344877a967e2255cf077bbfeb3a6371ee52
sande <sandeep.komera@gmail.com>
2018-11-03 10:46:01 +0530

    Welcome message to all added new package com.slokam.sandeep and a myproject.java

---
+++ src/main/java/com/slokam/sandeep/MyProject.java
@@ -0,0 +1,10 @@
+package com.slokam.sandeep;
+
+public class MyProject {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Friends welcome to the new concept of git");
+	}
+
+}
