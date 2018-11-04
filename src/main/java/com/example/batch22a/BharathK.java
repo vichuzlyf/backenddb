e79d02bf5b5b51f9cadeb911e1c074a1ce95d42d
karantalasila <karantalasila@gmail.com>
2018-11-04 12:15:00 +0530

    karan commited

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -1,6 +1,10 @@
 package com.example.batch22a;
 
 public class BharathK {
+	
+	public static void main(String[] args) {
+		
+	
 
 	public void harish() {
 		System.out.println("Harish ...");
@@ -9,22 +13,27 @@ public class BharathK {
 		System.out.println("I am bharath");
 	}
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	public void test2() {
 		
 		System.out.println("I am karan in bharath class");
-=======
+
 
 	public void santhosh()
 	{
 	}
->>>>>>> 1a37f7ed5b91a8cbbd98da223a950d07ab04c5e8
-=======
+
 	public void sandeep() {
 		System.out.println("Hello folks");
->>>>>>> e6a530fd6bbeb0f12535de38f7ef7ef87ea226ea
+
+	}
+	
+	public void karan() {
+	
+		System.out.println("we are in class on a sunday");
+	}
 	}
 }
+	}
  
  
