0fd30507482fff84805356c560800dcf60c853e0
veeraprathap9119 <prathap.mech70@gmail.com>
2018-11-04 09:31:43 -0800

    ddhsfgsdkjfghd

--- src/main/java/com/example/batch22b/Bharath.java
+++ src/main/java/com/example/batch22b/Bharath.java
@@ -4,14 +4,6 @@ public class Bharath {
 
 	
 	public void test1() {
-<<<<<<< HEAD
-		System.out.println("I am sharath");
-=======
-		System.out.println("Hello");
->>>>>>> ac70274e67a4115e8b381d738bf76706be9a1038
-	}
-	public void testone() {
-		System.out.println("Slokam");
-		
+		System.out.println("I am bharath");
 	}
 }
