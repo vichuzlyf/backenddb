c481cef017a821e9f3f87033e0361791d6fae7e2
Harish <mharish342@gmail.com>
2018-11-04 21:44:38 +0530

    add

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -1,7 +1,32 @@
 package com.example.batch22a;
 
 public class BharathK {
+	
+	public void shravya() {
+		System.out.println("shravya here");
+	}
 
+	public void shravyarao() {
+		System.out.println("hi there");
+	}
 
+	public static void main(String[] args) {
+		System.out.println("only");
+	}
+
+	public void Sai() {
+		System.out.println("Master nagasai...");
+	}
+
+	public void Raghu() {
+		System.out.println("GoodAfternoon...");
+	}
+
+	public void sunday() {
+		System.out.println("Happy Sunday...!!!");
+	}
+
+	public void harish() {
+		System.out.println("Hi i am from slokam tech...");
+	}
 }
-
