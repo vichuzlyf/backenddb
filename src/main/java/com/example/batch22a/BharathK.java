2456074b7685d53ba5002fbcecf8fcf81a0fd16c
rutu1514 <rutumendhe@gmail.com>
2018-11-04 12:50:05 +0530

    aa

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -2,5 +2,13 @@ package com.example.batch22a;
 
 public class BharathK {
 
-
+	public static void main(String[] args) {
+		
+		
+	}
+		
+	public void rutuNew()
+	{
+		System.out.println("rutu....");
+	}
 }
