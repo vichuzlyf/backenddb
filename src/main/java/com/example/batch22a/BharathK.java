79be01dfae19161adcbf487b82781847a7546114
arjaraghav <sai.raghavendra4522@gmail.com>
2018-11-04 00:21:38 -0700

    raghu

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -2,26 +2,25 @@ package com.example.batch22a;
 
 public class BharathK {
 
-<<<<<<< HEAD
 	
 	public void test1() {
 		System.out.println("I am bharath");
 	}
-}
+
  
  
-=======
 
 	public static void main(String[] args) {
 		
 
-<<<<<<< HEAD
->>>>>>> ad8ad90cf1527df4bd0262829b5c41dfc3b53e08
-=======
 		
 	}public void hari(){
 		System.err.println("welcome too slokam...");
-}
 
+
+
+	}
+   void add() {
+	   System.out.println("add");
+   }
 }
->>>>>>> 051c04ce49809f327f7e5e3f1bdb2a076647838d
