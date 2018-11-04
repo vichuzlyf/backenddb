9c2d67e3b117fab314d933665c1ba28db50ccc62
manoj <mk19745@gmail.com>
2018-11-04 12:18:27 +0530

    manojs

--- src/main/java/com/example/batch22a/BharathK.java
+++ src/main/java/com/example/batch22a/BharathK.java
@@ -2,10 +2,72 @@ package com.example.batch22a;
 
 public class BharathK {
 
+	public static void main(String[] args) {
+
+		public void harish() {
+			System.out.println("Harish ...");
+		}
+		public void test1() {
+			System.out.println("I am bharath ");
+			System.out.println("haiii sir");
+		}
+		public void hari(){
+			System.out.println("hari");
+		}
+
+		public void narasimha() {
+			System.out.println("I am narasimha reddy");
+		}
+		public void ghani(){
+			System.out.println("iam ghani");
+
+		}
+
+
+		public void methodByVijay() {
+			System.out.println("method added by vijay and commit through terminal...");
+		}
+		public void santhosh()
+		{
+		}
+
+
+
+		public void saiPurushothamReddy() {
+			System.out.println("SAI ANE NENU");
+		}
+		
+		public void test2() {
+		
+		}
+			
+			
+
+		public void viswanath(){
+			System.out.println("haiiii i am viswanath");
+
+
+			System.out.println("I am karan in bharath class");
+		}
+
+
+		
+
+		public void sandeep() {
+			System.out.println("Hello folks");
+
+		}
+		public void sample()
+		{
+			System.out.println("sampleeeeeee");
+		}
+		
+		public void Raghavendra() {
+			System.out.println("Hello ");
+		}
+		
 	
-	public void test1() {
-		System.out.println("I am bharath");
+
 	}
+
 }
- 
- 
---
+++ src/main/java/com/example/batch22a/manoj.java
@@ -0,0 +1,8 @@
+package com.example.batch22a;
+
+public class manoj {
+	public void manoj() {
+		System.out.println("advance many more happyreturns of d day suresh");
+	}
+
+}
